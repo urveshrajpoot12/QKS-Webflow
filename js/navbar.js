f3b487b68df6f8986ceeea5f8e3c01d574ab3740
URVESHRAJP00T <urveshr@quadrant-solutions.com>
2026-07-19 16:18:05 +0530

    updated both

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,22 @@
-console.log("Navbar Loaded");const dropdown = document.querySelector(".enterprise-dropdown");
+const dropdown = document.querySelector(".enterprise-dropdown");
 const menu = document.querySelector(".enterprise-menu");
 
-dropdown.addEventListener("mouseenter",()=>{
-
-    menu.classList.add("active");
-
+dropdown.addEventListener("mouseenter", () => {
+  menu.classList.add("active");
 });
 
-dropdown.addEventListener("mouseleave",()=>{
-
-    menu.classList.remove("active");
-
+dropdown.addEventListener("mouseleave", () => {
+  setTimeout(() => {
+    if (!menu.matches(":hover")) {
+      menu.classList.remove("active");
+    }
+  }, 100);
 });
 
-menu.addEventListener("mouseenter",()=>{
-
-    menu.classList.add("active");
-
+menu.addEventListener("mouseenter", () => {
+  menu.classList.add("active");
 });
 
-menu.addEventListener("mouseleave",()=>{
-
-    menu.classList.remove("active");
-
+menu.addEventListener("mouseleave", () => {
+  menu.classList.remove("active");
 });
