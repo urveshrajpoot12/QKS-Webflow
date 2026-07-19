f38b61295429562184a960f0db72acc2a819bb06
URVESHRAJP00T <urveshr@quadrant-solutions.com>
2026-07-19 16:08:33 +0530

    update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,26 @@
-console.log("Navbar Loaded");
+console.log("Navbar Loaded");const dropdown = document.querySelector(".enterprise-dropdown");
+const menu = document.querySelector(".enterprise-menu");
+
+dropdown.addEventListener("mouseenter",()=>{
+
+    menu.classList.add("active");
+
+});
+
+dropdown.addEventListener("mouseleave",()=>{
+
+    menu.classList.remove("active");
+
+});
+
+menu.addEventListener("mouseenter",()=>{
+
+    menu.classList.add("active");
+
+});
+
+menu.addEventListener("mouseleave",()=>{
+
+    menu.classList.remove("active");
+
+});
