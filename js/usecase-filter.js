529b4a2424fc5ec759fb7958124f3dcc95d55985
URVESHRAJP00T <urveshr@quadrant-solutions.com>
2026-07-29 12:35:35 +0530

    Read CMS Cards

--- js/usecase-filter.js
+++ js/usecase-filter.js
@@ -1,5 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("QKS Filter Loaded");
+    // Sabhi cards select karo
+    const cards = document.querySelectorAll("[data-card='true']");
+
+    console.log("Total Cards :", cards.length);
+
+    cards.forEach((card, index) => {
+
+        const objectives = card.querySelector("[data-objectives='true']");
+        const business = card.querySelector("[data-business='true']");
+
+        console.log("---------------");
+
+        console.log("Card :", index + 1);
+
+        console.log(
+            "Objectives :",
+            objectives ? objectives.textContent.trim() : "Not Found"
+        );
+
+        console.log(
+            "Business :",
+            business ? business.textContent.trim() : "Not Found"
+        );
+
+    });
 
 });
